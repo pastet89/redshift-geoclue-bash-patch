5f07e5ae9093b544feef4052bec7e05893335ca2
niki <phpshindaroff@gmail.com>
2019-01-11 09:55:13 +0200

    internet status check added

--- redshift.sh
+++ redshift.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+max_timeout = 5; #Max time in seconds waiting for an internet connection
+
 running_check() {
     if [[ $1 == *"redshift -l"* ]]; then
         eval $(echo msg_title="Success")
@@ -15,10 +17,19 @@ running_check() {
 }
 : '
 Provided that the script can be run immediately after
-the computer startup, wait few seconds till we are sure
+the computer startup, wait max_timeout seconds trying to verify that
 the internet connection is established.
 '
-sleep 5
+connected="false"; i=0;
+while [ "${i}" -lt "${max_timeout}" ]; do
+	ping -c1 -W1 4.4.4.4 >/dev/null;
+	if [ "$?" -eq 0 ]; then
+		connected="true";
+		break;
+	fi
+	i=$(( $i + 1 ));
+done
+
 redshift -l $(curl ipinfo.io | jq .loc | tr ',' ':' | tr '"' ' ') &
 : 'Sleep 3 more seconds after trying to run it before checking if it is
 running to make sure it has not crashed just after the initial startup'
